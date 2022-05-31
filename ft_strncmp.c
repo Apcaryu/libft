3edcd65ed9362ee621aa3123053f0bb977bff628
Antoine PELLEGRIN <apellegr@e3r12p17.clusters.42paris.fr>
2022-05-31 14:52:19 +0200

    Fix ft_strncmp function

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:42:06 by apellegr          #+#    #+#             */
-/*   Updated: 2022/05/04 15:17:47 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/05/31 14:50:23 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	cntr = 0;
 	if ((s1[0] != '\0' || s2[0] != '\0') && cntr < n)
 	{
-		while ((s1[cntr] || s2[cntr]) && cntr < n)
+		while (((unsigned char)s1[cntr] || (unsigned char)s2[cntr]) && cntr < n)
 		{
-			if (s1[cntr] != s2[cntr])
+			if ((unsigned char)s1[cntr] != (unsigned char)s2[cntr])
 			{
-				return (s1[cntr] - s2[cntr]);
+				return ((unsigned char)s1[cntr] - (unsigned char)s2[cntr]);
 			}
 			cntr++;
 		}
