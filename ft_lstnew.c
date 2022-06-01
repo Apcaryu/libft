bdbeda4f01cec4c3cc29e861c4662feaee66e5c5
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 15:06:46 +0200

    Change name of ft_lstnew file

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,9 +6,20 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:50:34 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 14:50:47 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 15:05:21 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 t_list	*ft_lstnew(void *content)
-{}
+{
+	t_list *new_list;
+
+	new_list = malloc(sizeof(t_list));
+	if (new_list == NULL)
+		return (NULL);
+	new_list->content = content;
+	new_list->next = NULL;
+	return (new_list);
+}
