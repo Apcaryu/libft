fcf0f98660bc743a8f47fd083844c95bf4593867
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 15:29:07 +0200

    finish ft_lstadd_front function

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,11 +6,14 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:11:56 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 15:15:06 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 15:27:21 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
-{}
+{
+	new->next = *lst;
+	*lst = new;
+}
