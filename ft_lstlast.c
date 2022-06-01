38e5263c52b95aad18ffb419bbca510dcfaf4fb5
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 16:15:16 +0200

    finish ft_lstlast function

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,11 +6,23 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:06:29 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 16:07:17 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 16:13:45 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
-{}
+{
+	t_list	*ptr;
+	t_list *tmp;
+
+	tmp = lst;
+	ptr = tmp;
+	while (tmp)
+	{
+		ptr = tmp;
+		tmp = tmp->next;
+	}
+	return (ptr);
+}
