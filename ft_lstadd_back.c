c12bcd665accc7d16be538af61183164eda78fd7
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 17:49:00 +0200

    finish ft_lstadd_back function

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,11 +6,20 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:20:40 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 16:21:17 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 17:37:12 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 void	ft_lstadd_back(t_list **lst, t_list *new)
-{}
+{
+	t_list *last;
+	if (!(*lst))
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
