392eebac120c61866531019d7826b761e7685006
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 15:38:18 +0200

    finish ft_lstsize function

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,11 +6,23 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:30:44 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 15:31:04 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 15:37:02 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int		ft_lstsize(t_list *list)
-{}
+{
+	int cntr;
+	t_list *ptr;
+
+	cntr = 0;
+	ptr = list;
+	while (ptr)
+	{
+		cntr++;
+		ptr = ptr->next;
+	}
+	return (cntr);
+}
