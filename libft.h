0152943c8c9e721e12e01aec926c9777e4928970
Antoine PELLEGRIN <apellegr@e3r10p19.clusters.42paris.fr>
2022-06-01 15:31:25 +0200

    add ft_lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 10:30:49 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 15:11:17 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/01 15:30:14 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *list);
 
 #endif
