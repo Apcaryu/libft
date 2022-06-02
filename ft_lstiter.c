13725c42ff9927b4528116baa6f4067cf8049d2d
Antoine PELLEGRIN <apellegr@e3r12p17.clusters.42paris.fr>
2022-06-02 17:33:55 +0200

    finish ft_lstiter function

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,11 +6,23 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:27:30 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/02 17:28:01 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/02 17:33:19 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
-{}
+{
+	t_list *tmp;
+
+	tmp = lst;
+	if (lst && f)
+	{
+		while (tmp)
+		{
+			f(tmp->content);
+			tmp = tmp->next;
+		}
+	}
+}
