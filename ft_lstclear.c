a4ebea98a544cbcce209fbbd584827ff62e07db5
Antoine PELLEGRIN <apellegr@e3r12p17.clusters.42paris.fr>
2022-06-02 16:29:47 +0200

    finish ft_lstclean function

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,25 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 18:19:35 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 18:20:09 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/02 16:28:53 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
-{}
+{
+	t_list	*target;
+
+	if (lst)
+	{
+		target = (*lst)->next;
+		while (target)
+		{
+			target = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			*lst = target;
+		}
+		lst = NULL;
+	}
+}
