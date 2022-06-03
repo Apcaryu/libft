712d3fac47cb2a37892b3a436c53a2ff1f62dd7f
Apcaryu <antoinepellegrin@outlook.fr>
2022-06-03 02:46:14 +0200

    complete ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -13,4 +13,24 @@
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{}
+{
+	t_list 	*tmp;
+	t_list	*lstout;
+
+	if (lst && f && del)
+	{
+		while (lst)
+		{
+			tmp = ft_lstnew(lst->content);
+			if (!tmp)
+			{
+				ft_lstclear(&lstout, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&lstout, tmp);
+			lst = lst->next;
+		}
+		return (lstout);
+	}
+	return (NULL);
+}
