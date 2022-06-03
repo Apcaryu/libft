f673e5381c1aa32766f596740e6a8911a09e7734
Antoine PELLEGRIN <apellegr@e3r11p22.clusters.42paris.fr>
2022-06-03 17:43:29 +0200

    norm ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,16 +6,16 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:30:44 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/01 15:37:02 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/03 17:43:11 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_lstsize(t_list *list)
+int	ft_lstsize(t_list *list)
 {
-	int cntr;
-	t_list *ptr;
+	int		cntr;
+	t_list	*ptr;
 
 	cntr = 0;
 	ptr = list;
