11749d5631df5d7967ed78e7063edeeae0495d3d
Antoine PELLEGRIN <apellegr@e3r11p22.clusters.42paris.fr>
2022-06-03 17:22:17 +0200

    finish ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:43:17 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/02 17:44:00 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/03 17:21:42 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,29 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list 	*tmp;
+	t_list	*tmp;
 	t_list	*lstout;
 
 	if (lst && f && del)
 	{
-		while (lst)
+		tmp = ft_lstnew(f(lst->content));
+		if (tmp)
 		{
-			tmp = ft_lstnew(lst->content);
-			if (!tmp)
-			{
-				ft_lstclear(&lstout, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&lstout, tmp);
+			lstout = tmp;
 			lst = lst->next;
+			while (lst)
+			{
+				tmp = ft_lstnew(f(lst->content));
+				if (!tmp)
+				{
+					ft_lstclear(&lstout, del);
+					return (NULL);
+				}
+				ft_lstadd_back(&lstout, tmp);
+				lst = lst->next;
+			}
+			return (lstout);
 		}
-		return (lstout);
 	}
 	return (NULL);
 }
