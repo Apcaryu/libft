61eb3538e2ffba0bc2b59f0d373c7e9b8f1a39b7
Antoine PELLEGRIN <antoinepellegrin@outlook.fr>
2022-06-22 17:49:19 +0200

    add check test in ft_lstadd_back function

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: apellegr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:20:40 by apellegr          #+#    #+#             */
-/*   Updated: 2022/06/03 17:32:23 by apellegr         ###   ########.fr       */
+/*   Updated: 2022/06/22 17:47:43 by apellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
-
-	if (!(*lst))
-		*lst = new;
-	else
+	if (lst)
 	{
-		last = ft_lstlast(*lst);
-		last->next = new;
+		if (!(*lst))
+			*lst = new;
+		else
+		{
+			last = ft_lstlast(*lst);
+			last->next = new;
+		}
 	}
 }
